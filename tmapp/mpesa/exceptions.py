27fd529dbbd5d0246c87412d25745093ab6da11a
tingachris <chrispinetinega@gmail.com>
2025-03-08 09:14:48 +0300

    refactor MPESA exception classes to inherit from MpesaError and improve documentation

--- tmapp/mpesa/exceptions.py
+++ tmapp/mpesa/exceptions.py
@@ -4,35 +4,30 @@ Exceptions raised during usage of the MPESa API
 
 class MpesaError(Exception):
 	"""
-	Raised for a general error regarding the library
-	"""
-
-class IllegalPhoneNumberException(Exception):
-	"""
-	Raised when phone number is in illegal format.
+	Raised for a general error regarding the library.
 	"""
 	pass
 
-
-class MpesaConnectionError(Exception):
+class IllegalPhoneNumberException(MpesaError):
 	"""
-	Raised when connection has an error
-	"""
-	pass
-
-class MpesaConfigurationException(Exception):
-	"""
-	Raised when Mpesa environment variables are not configured properly
+	Raised when the phone number is in an illegal format.
 	"""
 	pass
 
-class MpesaInvalidParameterException(Exception):
+class MpesaConnectionError(MpesaError):
 	"""
-	Raised when an an invalid parameter is passed in a function call
-	
-	Extends:
-		Exception: Exception
+	Raised when there is a connection error.
 	"""
 	pass
 
+class MpesaConfigurationException(MpesaError):
+	"""
+	Raised when Mpesa environment variables are not configured properly.
+	"""
+	pass
 
+class MpesaInvalidParameterException(MpesaError):
+	"""
+	Raised when an invalid parameter is passed in a function call.
+	"""
+	pass
